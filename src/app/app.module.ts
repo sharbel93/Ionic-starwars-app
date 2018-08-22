55822e58c92a077e3f5b02bae13be307e7207d0f
sharbel93 <sharbel.chris@gmail.com>
2018-08-22 14:15:05 +0300

    Add tab_bar,change views,using NavController, Passing parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,12 +6,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { TabsPage } from './../pages/tabs/tabs';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,7 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    TabsPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/films/films.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FilmsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-films',
+  templateUrl: 'films.html',
+})
+export class FilmsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  openDetails() {
+    this.navCtrl.push('FilmDetailsPage');
+  }
+
+  goToPlanets(){
+    this.navCtrl.parent.select(2);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FilmsPage');
+  }
+
+}
